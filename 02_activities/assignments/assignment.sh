315604ae160a35e8f8eb5778668150904cb2801f
Seo Young Lee <seo.lee@mail.utoronto.ca>
2026-03-04 00:41:18 -0500

    restore inventory.txt in the correct data directory

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -46,7 +46,7 @@ cp data/raw/*user*.log data/processed/user_logs/
 rm data/raw/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls -R ../processed > ../inventory.txt
+ls -R data/processed > data/inventory.txt
 ###########################################
 echo "Project setup is complete!"
 echo "Project setup is complete!"
